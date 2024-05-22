9dc25d67b85e47d19bc073ad91fb5a94d23533ab
Sanpri <Sanpri@LAPTOP-V2EFQHVH>
2024-05-22 10:11:25 +0530

    new product framework

---
+++ GitDemo/src/test/java/ProductTEST/CreateProductTest.java
@@ -0,0 +1,11 @@
+package ProductTEST;
+
+import org.testng.annotations.Test;
+
+public class CreateProductTest {
+
+	@Test
+	public void createProductTest() {
+	System.out.println("product page");	
+	}
+}
